484a3dd2e13011777127a8951afdc37db167643f
unni <sri654jith@gmail.com>
2021-06-16 19:03:05 +0530

    sol2

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,6 @@
-delete from books_subjects where subject in (select subject from books_subjects where subject=8);
+delete from books_subjects where subject in (select bs.subject from books_subjects bs, subjects s
+where
+bs.subject=s.id
+and
+s.name='History');
 delete from subjects where name="History";
